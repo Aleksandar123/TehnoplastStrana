72127dda3fa4055ad98f263714eda500ed5119fc
Aleksandar <aleksandar.nikolov1@gmail.com>
2016-05-11 13:25:47 +0200

    Current Version of the website

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(document).ready(function()
     $('.flicker-example').flickerplate({
 		auto_flick: true,
         auto_flick_delay: 5,
+		arrows: true,
 	});
 	
 });
+
+console.log('%c Made by Aleksandar aleksandar.nikolov1@gmail.com ', 'background: #222; color: #bada55');
